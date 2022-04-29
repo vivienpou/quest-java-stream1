813498fc17820d4d29cfd26e83a8f8bbd6858049
VivienPou <38348371+vivienpou@users.noreply.github.com>
2022-04-29 12:12:22 +0200

    update the code

--- Shield.java
+++ Shield.java
@@ -4,8 +4,16 @@ import java.util.stream.Collectors;
 
 
 public class Shield {
+	public static List<Hero> filterHeroesAge (List<Hero> heroes, Predicate<Hero> predicateAge){
+		return heroes.stream().filter(predicateAge).collect(Collectors.<Hero>toList());
+	}
 
-    public static void main(String[] args) {
+	public static List<Hero> filterHeroesGlutenIntolerance (List<Hero> heroes, Predicate<Hero> predicateGlutenIntolerance){
+		return heroes.stream().filter(predicateGlutenIntolerance).collect(Collectors.<Hero>toList());
+	}
+
+
+	public static void main(String[] args) {
 
         List<Hero> heroes = new ArrayList<>();
         heroes.add(new Hero("Black Widow", 34, false));
@@ -17,19 +25,20 @@ public class Shield {
         heroes.add(new Hero("Spider-Man", 18, false));
         heroes.add(new Hero("Hulk", 49, true));
         heroes.add(new Hero("Doctor Strange", 42, false));
-
         List<Hero> elders;
-        elders = heroes.stream().filter(h -> h.getAge() > 59).collect(Collectors.toList());
-        // TODO 1 : filter heroes in order to found heroes older than 59
 
+        // TODO 1 : filter heroes in order to found heroes older than 59
+		Predicate<Hero> predicateAge = h -> h.getAge()>59;
+		elders = filterHeroesAge(heroes, predicateAge);
         System.out.println("\nElders:");
         for (Hero elder : elders) {
             System.out.println(elder.getName());
         }
 
         List<Hero> intolerants;
-        intolerants = heroes.stream().filter(h->h.isGlutenIntolerant()).collect(Collectors.toList());
         // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
+		Predicate<Hero> predicateGlutenIntolerance = h -> h.isGlutenIntolerant();
+		intolerants = filterHeroesGlutenIntolerance(heroes,predicateGlutenIntolerance);
 
         System.out.println("\nGluten intolerants:");
         for (Hero intolerant : intolerants) {
